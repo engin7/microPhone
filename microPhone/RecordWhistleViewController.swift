dc2449f8e3783113b42cc93fa66d7846bb0f0159
Engin <kuk.engin@gmail.com>
2021-08-25 12:09:24 +0300

    UI updates

--- microPhone/RecordWhistleViewController.swift
+++ microPhone/RecordWhistleViewController.swift
@@ -13,7 +13,8 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
 
     var onSaveRecording: ((URL) -> Void)?
     var stackView: UIStackView!
-    var horizontalSV: UIStackView!
+    var horizontalSV0: UIStackView!
+    var horizontalSV1: UIStackView!
 
     var recordButton: UIButton!
     var recordImageView: UIImageView!
@@ -39,7 +40,7 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
         view = UIView()
 
         view.backgroundColor = UIColor.lightGray
-
+        
         stackView = UIStackView()
         stackView.backgroundColor = .white
         stackView.spacing = 20
@@ -51,8 +52,18 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
 
         stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        stackView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height/3).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 350).isActive = true
+        
+        let bottomView = UIView()
+        view.addSubview(bottomView)
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        bottomView.backgroundColor = .white
+        bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
     }
     
     func startRecordingSession() {
@@ -83,12 +94,17 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 40),
+            titleLabel.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 50),
         ])
         
         recordImageView = UIImageView()
         stackView.addArrangedSubview(recordImageView)
-  
+        recordImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            recordImageView.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.6),
+            recordImageView.heightAnchor.constraint(equalToConstant: 150),
+        ])
+        
         seekBar = UISlider()
         seekBar.translatesAutoresizingMaskIntoConstraints = false
         stackView.addArrangedSubview(seekBar)
@@ -102,17 +118,17 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
         seekBar.minimumValue = 0
         seekBar.maximumValue = 100
         
-        horizontalSV = UIStackView()
-        horizontalSV.backgroundColor = .white
-        horizontalSV.spacing = 40
-        horizontalSV.translatesAutoresizingMaskIntoConstraints = false
-        horizontalSV.distribution = UIStackView.Distribution.fillEqually
-        horizontalSV.alignment = .center
-        horizontalSV.axis = .horizontal
-        stackView.addArrangedSubview(horizontalSV)
+        horizontalSV0 = UIStackView()
+        horizontalSV0.backgroundColor = .white
+        horizontalSV0.spacing = 40
+        horizontalSV0.translatesAutoresizingMaskIntoConstraints = false
+        horizontalSV0.distribution = UIStackView.Distribution.fillEqually
+        horizontalSV0.alignment = .center
+        horizontalSV0.axis = .horizontal
+        stackView.addArrangedSubview(horizontalSV0)
  
         playButton = UIButton()
-        horizontalSV.addArrangedSubview(playButton)
+        horizontalSV0.addArrangedSubview(playButton)
         playButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
         playButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         playButton.layer.shadowOpacity = 1.0
@@ -123,11 +139,46 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
         recordButton = UIButton()
         recordButton.setImage(#imageLiteral(resourceName: "check"), for: .normal)
         recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
-        horizontalSV.addArrangedSubview(recordButton)
-         
-        let gapView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 20))
-        gapView.backgroundColor = .white
-        stackView.addArrangedSubview(gapView)
+        horizontalSV0.addArrangedSubview(recordButton)
+        
+//        let gapView = UIView()
+//        stackView.addArrangedSubview(gapView)
+//        gapView.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            gapView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+//            gapView.heightAnchor.constraint(equalToConstant: 10),
+//        ])
+//
+        horizontalSV1 = UIStackView()
+        horizontalSV1.backgroundColor = .white
+        horizontalSV1.spacing = 20
+        horizontalSV1.translatesAutoresizingMaskIntoConstraints = false
+        horizontalSV1.distribution = UIStackView.Distribution.fillEqually
+        horizontalSV1.alignment = .center
+        horizontalSV1.axis = .horizontal
+        stackView.addArrangedSubview(horizontalSV1)
+        horizontalSV1.isHidden = true
+
+        let cancelButton = UIButton()
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.backgroundColor = .lightGray
+        cancelButton.layer.cornerRadius = 5
+        horizontalSV1.addArrangedSubview(cancelButton)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let saveButton = UIButton()
+        saveButton.setTitle("SAVE", for: .normal)
+        saveButton.backgroundColor = .blue.withAlphaComponent(0.8)
+        saveButton.layer.cornerRadius = 5
+        horizontalSV1.addArrangedSubview(saveButton)
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            cancelButton.widthAnchor.constraint(equalToConstant: 150),
+            cancelButton.heightAnchor.constraint(equalToConstant: 50),
+            saveButton.widthAnchor.constraint(equalToConstant: 150),
+            saveButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
         
         startRecording()
     }
@@ -144,6 +195,7 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
     @objc func recordTapped() {
         if soundRecorder == nil {
             startRecording()
+            horizontalSV1.isHidden = true
             seekBar.isHidden = true
             recordImageView.isHidden = false
         } else {
@@ -165,6 +217,8 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
     }
     
     func startRecording() {
+        timer?.invalidate()
+        seekBar.setValue(seekBar.minimumValue, animated: false)
         titleLabel.text = "We are recording now"
         recordImageView.image = #imageLiteral(resourceName: "recording")
 
@@ -196,14 +250,14 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
         }
     }
     
-    func showSeekBar() {
-        
+    func updateUI() {
         seekBar.isHidden = false
         recordImageView.isHidden = true
+        horizontalSV1.isHidden = false
     }
     
     func finishRecording(success: Bool) {
-
+        
         soundRecorder.stop()
         recordUrl = soundRecorder.url
         soundRecorder = nil
@@ -214,7 +268,7 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
             titleLabel.text = "You can play your record"
             playButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
             
-            showSeekBar()
+            updateUI()
 
             playButton.removeTarget(nil, action: nil, for: .allEvents)
             playButton.addTarget(self, action: #selector(playTapped), for: .touchUpInside)
@@ -250,7 +304,7 @@ class AudioRecVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelega
         if seekBar.maximumValue - progress < 0.25 {
             seekBar.setValue(seekBar.maximumValue, animated: false)
             timer?.invalidate()
-            titleLabel.text = "End of recording"
+            titleLabel.text = "End of record"
             playButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
         } else {
             seekBar.setValue(progress, animated: false)
